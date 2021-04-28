ba357f29c789500d1d33170686f3e6a882a80636
Steve Datz <sfdatz@live.com>
2021-04-28 16:55:32 -0500

    Initial Commit

---
+++ 01_testProject/01_testProject/src/01_testProject.cpp
@@ -0,0 +1,23 @@
+// 01_testProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+#include <iostream>
+// TUTORIAL FOR SET UP https://www.youtube.com/watch?v=qeH9Xv_90KM
+// In Solution Explorer toggle "Show all Files" to hide weird folder options
+// Create your own src folder (right click)
+// put ur main in src
+// Right click project --> Properties -->Configuration Properties-> General :
+// Change To All Configure and All Platforms:
+// Change OUTPUT DIR : $(SolutionDir)\bin\$(Platform)\$(Configurations)
+// Change INTERMEDIATE DIR:  $(SolutionDir)\bin\$(Platform)\Intermediaries\$(Configurations)
+
+int main()
+{
+    // 2 diff ways to new Line/EndL?
+    std::cout << "Hello World!\n" << std::endl;
+}
+
+
+

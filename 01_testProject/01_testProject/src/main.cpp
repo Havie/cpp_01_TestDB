1562c15e20987f526b844c6f7be837d8182c252b
Steve Datz <sfdatz@live.com>
2021-04-29 12:02:25 -0500

    Sizes and maxs of common data types

--- 01_testProject/01_testProject/src/main.cpp
+++ 01_testProject/01_testProject/src/main.cpp
@@ -23,13 +23,18 @@ float annualSalary_;
 int main()
 {
     // 2 diff ways to new Line/EndLine
-    cout << "Enter annualSalary.. \n" << endl;
+    //cout << "Enter annualSalary.. \n" << endl;
 
-    cin >> annualSalary_;
-    float monthlySalary = annualSalary_ / 12;
-
-    cout << "your monthly salary is " << monthlySalary << endl;
-    cout << sizeof(annualSalary_) << " .. is size of float";
+    //cin >> annualSalary_;
+    //float monthlySalary = annualSalary_ / 12;
+    //cout << "your monthly salary is " << monthlySalary << endl;
+    cout << sizeof(unsigned int) << " .. is byte size of unsigned Int and max val is : " << UINT_MAX << endl ;
+    cout << sizeof(int) << " .. is byte size of int and max val is : " << INT_MAX << endl;
+    cout << sizeof(float) << " .. is byte size of float and max val is : " << numeric_limits<float>::max() <<  endl;
+    cout << sizeof(double) << " .. is byte size of double and max val is : " << numeric_limits<double>::max() << endl;
+    cout << sizeof(char) << " .. is byte size of char  and max val is : " << numeric_limits<char>::max() << endl;
+    cout << sizeof(long) << " .. is byte size of long  and max val is : " << numeric_limits<long>::max() << endl;
+    cout << sizeof(bool) << " .. is byte size of bool  and max val is : " << numeric_limits<bool>::max() << endl;
     //system("pause>0"); // pause prints the press any key message, the > tells it to redirect output to a file, 0 is the name of the file to redirect to
     cin.get(); //way 2 to pause
     return 0;

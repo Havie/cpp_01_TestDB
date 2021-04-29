21d7d2553a2cd728c89362174e7a8e6cd0c2a5e3
Steve Datz <sfdatz@live.com>
2021-04-29 11:55:36 -0500

    Size of and naming conventions

--- 01_testProject/01_testProject/src/main.cpp
+++ 01_testProject/01_testProject/src/main.cpp
@@ -2,6 +2,8 @@
 #include "main.h"
 #include <iostream>
 #include <process.h>
+using namespace std; //Stop having to do std::cout
+
 // TUTORIAL FOR SET UP https://www.youtube.com/watch?v=qeH9Xv_90KM
 // In Solution Explorer toggle "Show all Files" to hide weird folder options
 // Create your own src folder (right click)
@@ -13,13 +15,22 @@
 
 //SRC Control add: *.opensdf and * .opendb to fix fatalError to gitignore
 
-
 // TUTORIAL 10hour:  https://www.youtube.com/watch?v=GQp1zzTwrIg
+
+///C++ member vars end w _ 
+float annualSalary_;
+
 int main()
 {
-    // 2 diff ways to new Line/EndL?
-    std::cout << "Hello World!\n" << std::endl;
-    system("pause>0"); //way 1 to pause
-    std::cin.get(); //way 2 to pause
+    // 2 diff ways to new Line/EndLine
+    cout << "Enter annualSalary.. \n" << endl;
+
+    cin >> annualSalary_;
+    float monthlySalary = annualSalary_ / 12;
+
+    cout << "your monthly salary is " << monthlySalary << endl;
+    cout << sizeof(annualSalary_) << " .. is size of float";
+    //system("pause>0"); // pause prints the press any key message, the > tells it to redirect output to a file, 0 is the name of the file to redirect to
+    cin.get(); //way 2 to pause
     return 0;
 }
